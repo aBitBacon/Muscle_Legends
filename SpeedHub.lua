eb23359197fad6724c9553742ed7989112543b1a
aBitBacon <Jadexrice@gmail.com>
2025-04-15 16:01:50 -0500

    Update SpeedHub.lua

--- SpeedHub.lua
+++ SpeedHub.lua
@@ -1,1 +1,10 @@
 
+-- Speed Hub Script By Ahmad!
+
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
+    end
+end
